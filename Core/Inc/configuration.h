c8744abd701e6d7bef0071e3d87671180de7c57f
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-10-27 18:34:22 -0400

    Add initial maze navigation code

---
+++ Core/Inc/configuration.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * @file configuration.h <- TODO: DEV CONFIGURATIONS!
+ * @brief High level hardcoded configuration related declarations/definitions.
+ *******************************************************************************
+ */
+
+#ifndef MAZE_BOT__CONFIGURATION_H
+#define MAZE_BOT__CONFIGURATION_H
+
+/** Includes. *****************************************************************/
+
+#include "math.h"
+
+/** Configuration definitions. ************************************************/
+
+#define MAZE_BOT_NOMINAL_MAZE_WIDTH_MM 155.0f
+#define MAZE_BOT_TOF_ANGLE_RAD (0.25f * M_PI)
+
+#endif
---
+++ Core/Inc/maze_navigation.h
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * @file maze_navigation.h
+ * @brief Maze navigation software.
+ *******************************************************************************
+ */
+
+#ifndef MAZE_BOT__MAZE_NAVIGATION_H
+#define MAZE_BOT__MAZE_NAVIGATION_H
+
+/** Public functions. *********************************************************/
+
+float heading_error_rad(float left_mm, float right_mm, float angle_rad);
+
+#endif
