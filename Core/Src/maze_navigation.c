c2a1560e28e5b16109f290082b16111edc48a689
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-10-28 01:58:44 -0400

    Initial heading error calculation

--- Core/Src/maze_navigation.c
+++ Core/Src/maze_navigation.c
@@ -15,6 +15,8 @@
 
 /** Public variables. *********************************************************/
 
+volatile float heading_error_rad_calc = 0;
+
 /** Private functions. *********************************************************/
 
 static float acos_clamped_rad(float x) {
@@ -29,13 +31,15 @@ static float acos_clamped_rad(float x) {
 static float compute_line_from_points(const float left_mm, const float right_mm,
                                       const float angle_rad) {
   return sqrtf(left_mm * left_mm + right_mm * right_mm -
-               2 * left_mm * right_mm * cosf(M_PI - 2 * angle_rad));
+               2 * left_mm * right_mm * cosf(angle_rad));
 }
 
 /** Public functions. *********************************************************/
 
 float heading_error_rad(const float left_mm, const float right_mm,
                         const float angle_rad) {
-  const float line = compute_line_from_points(left_mm, right_mm, angle_rad);
+  const float line = compute_line_from_points(
+      left_mm + MAZE_BOT_TOF_LEFT_RIGHT_OFFSET_MM,
+      right_mm + MAZE_BOT_TOF_LEFT_RIGHT_OFFSET_MM, M_PI - 2 * angle_rad);
   return acos_clamped_rad(MAZE_BOT_NOMINAL_MAZE_WIDTH_MM / line);
 }
