1c6890433b39f2bb3d7122c78dafebc89d10b8ff
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-10-27 23:03:33 -0400

    Add configuration macros for public TOF variables

--- Core/Inc/configuration.h
+++ Core/Inc/configuration.h
@@ -13,7 +13,13 @@
 
 /** Configuration definitions. ************************************************/
 
+// Maze navigation related constraints.
 #define MAZE_BOT_NOMINAL_MAZE_WIDTH_MM 155.0f
 #define MAZE_BOT_TOF_ANGLE_RAD (0.25f * M_PI)
+
+// Dev related features:
+
+// Define to use direct public TOF (distance_mm) data.
+// #define MAZE_BOT_VL53L4CD_PUBLIC_DIRECT_VAR
 
 #endif
