f0d8d3a86f190763d3666fea57629c76be72bea6
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-10-28 08:40:45 -0400

    Add minor fixes to drive kinematics

--- Core/Src/drive.c
+++ Core/Src/drive.c
@@ -6,6 +6,7 @@
 
 #include "drive.h"
 #include "h_bridge_control.h"
+#include "math.h"
 #include "stm32l4xx_hal_gpio.h"
 
 /** Definitions. **************************************************************/
@@ -14,6 +15,7 @@
 #define MAX_PWM_DUTY 100.0f
 #define MIN_PWM_DUTY 20.0f
 #define PWM_DUTY_DELTA (MAX_PWM_DUTY - MIN_PWM_DUTY)
+#define DRIVE_DEADBAND 0.02f
 
 /** Public variables. *********************************************************/
 
@@ -31,8 +33,8 @@ static uint16_t scale_pwm_duty(float drive_value) {
     drive_value = -1.0f;
 
   // Scale [-1,1] -> [MIN, MAX].
-  const float scaled =
-      ((drive_value + 1.0f) * 0.5f) * PWM_DUTY_DELTA + MIN_PWM_DUTY;
+  const float mag = fabsf(drive_value);
+  const float scaled = mag * PWM_DUTY_DELTA + MIN_PWM_DUTY;
 
   return (uint16_t)scaled;
 }
@@ -45,7 +47,7 @@ void drive(void) {
   const float h_bridge_1_drive = h_bridge_linear + h_bridge_1_yaw_control;
   const float h_bridge_2_drive = h_bridge_linear + h_bridge_2_yaw_control;
 
-  if (h_bridge_1_drive == 0) {
+  if (fabsf(h_bridge_1_drive) < DRIVE_DEADBAND) {
     h_bridge_1_command(GPIO_PIN_RESET, 0);
   } else {
     const GPIO_PinState pin_1_a_state =
@@ -53,7 +55,7 @@ void drive(void) {
     h_bridge_1_command(pin_1_a_state, scale_pwm_duty(h_bridge_1_drive));
   }
 
-  if (h_bridge_2_drive == 0) {
+  if (fabsf(h_bridge_2_drive) < DRIVE_DEADBAND) {
     h_bridge_2_command(GPIO_PIN_RESET, 0);
   } else {
     const GPIO_PinState pin_2_a_state =
