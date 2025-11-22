c3889a57f834bc54350c446c1f756955da9fc1ed
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-11-22 18:46:51 -0500

    Add vision message callback

--- Core/Inc/maze_navigation.h
+++ Core/Inc/maze_navigation.h
@@ -7,12 +7,22 @@
 #ifndef MAZE_BOT__MAZE_NAVIGATION_H
 #define MAZE_BOT__MAZE_NAVIGATION_H
 
+/** Includes. *****************************************************************/
+
+#include "../maze_bot_driver/maze_bot_can_dbc.h"
+
 /** Public variables. *********************************************************/
 
 extern volatile float heading_error_rad_calc;
 extern volatile float position_error_mm_calc;
+extern volatile uint16_t vision_x1;
+extern volatile uint16_t vision_y1;
+extern volatile uint16_t vision_x2;
+extern volatile uint16_t vision_y2;
 
 /** Public functions. *********************************************************/
+
+void process_vision(can_header_t *header, uint8_t *data);
 
 /**
  * @brief Computes the robot's heading error (in radians) relative to a wall
--- Core/Src/maze_navigation.c
+++ Core/Src/maze_navigation.c
@@ -21,6 +21,19 @@ typedef enum { IDLE, STANDBY, STRAIGHT, TURN, SETTLING } mode_t;
 
 volatile float heading_error_rad_calc = 0;
 volatile float position_error_mm_calc = 0;
+volatile uint16_t vision_x1 = 0;
+volatile uint16_t vision_y1 = 0;
+volatile uint16_t vision_x2 = 0;
+volatile uint16_t vision_y2 = 0;
+
+/** Public functions. *********************************************************/
+
+void process_vision(can_header_t *header, uint8_t *data) {
+  vision_x1 = data[0] & data[1] << 8;
+  vision_y1 = data[2] & data[3] << 8;
+  vision_x2 = data[4] & data[5] << 8;
+  vision_y2 = data[6] & data[7] << 8;
+}
 
 /** Private variables. ********************************************************/
 
